b1eee4d7e611fc0073ee31296ecf15ffcbf2852c
Lakshay-bhardwaj-neophyte <b321061@iiit-bh.ac.in>
2026-06-18 10:34:03 +0000

    terraform fmt

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,14 +21,14 @@ resource "azurerm_subnet" "appgw_subnet" {
 module "application_gateway" {
   source = "../../modules/application_gateway"
 
-  environment           = var.environment
-  resource_group_name   = data.azurerm_resource_group.rg.name
-  location              = data.azurerm_resource_group.rg.location
-  
+  environment         = var.environment
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+
   # Pass the ID of the newly created subnet!
   app_gateway_subnet_id = azurerm_subnet.appgw_subnet.id
-  
-  private_frontend_ip   = var.private_frontend_ip
-  backend_ip_address    = var.backend_ip_address
-  trusted_ips           = var.trusted_ips
+
+  private_frontend_ip = var.private_frontend_ip
+  backend_ip_address  = var.backend_ip_address
+  trusted_ips         = var.trusted_ips
 }
--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -7,13 +7,13 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "Sentinel-Inference-server"
-    
+    resource_group_name = "Sentinel-Inference-server"
+
     # PUT YOUR AZURE STORAGE ACCOUNT NAME HERE
-    storage_account_name = "sentinelv1" 
-    
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    storage_account_name = "sentinelv1"
+
+    container_name = "tfstate"
+    key            = "dev.terraform.tfstate"
   }
 }
 
