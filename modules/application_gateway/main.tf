e30250d31a97a29c7b0aacac5044333f7e3275eb
Lakshay-bhardwaj-neophyte <b321061@iiit-bh.ac.in>
2026-06-19 06:34:49 +0000

    "ssl policy"

--- modules/application_gateway/main.tf
+++ modules/application_gateway/main.tf
@@ -57,6 +57,11 @@ resource "azurerm_application_gateway" "appgw" {
     capacity = 2
   }
 
+  ssl_policy {
+    policy_type = "Predefined"
+    policy_name = "AppGwSslPolicy20220101"
+  }
+
   gateway_ip_configuration {
     name      = "appgw-ip-config"
     subnet_id = var.app_gateway_subnet_id
